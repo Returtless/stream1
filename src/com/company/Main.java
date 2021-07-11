bdf3ab9315ce8e65af395f4d5fd8c526b3830638
Vladislav Likhachev <mymindissharp@gmail.com>
2021-07-11 13:23:21 +0300

    Выполнение операций над числами

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.stream()
+                .filter(x -> x > 0 && x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
     }
 }
